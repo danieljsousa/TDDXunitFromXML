f22aeea3e7b85d6af39f05401ffc0641bcb1bfa8
danieljsousa <54507772+danieljsousa@users.noreply.github.com>
2019-08-25 13:18:19 -0400

    Update XmlDataTest.cs

--- XmlDataTest.cs
+++ XmlDataTest.cs
@@ -8,7 +8,7 @@ namespace FirstTest
     public class XmlDataTest
     {
         [Theory]
-        [XmlData(@"teste.xml","/teste","./valor1/text(),./valor2/text(),./@valor3")]
+        [XmlData(@"../../../teste.xml","/teste","./valor1/text(),./valor2/text(),./@valor3")]
         public void XMLDATAFuncionaTest(int x, int y,int z)
         {
             Assert.True((x + y + z) == 80);
